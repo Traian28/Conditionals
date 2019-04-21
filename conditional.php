73cbc14c739de4d0975cb5d8c8e70b9555f062ac
Traian Alimenti - Freelancer <traian.alimentibel@gmail.com>
2019-04-21 19:16:12 -0300

    Add files via upload

---
+++ conditional.php
@@ -0,0 +1,15 @@
+<?php
+
+$edad = 18;
+$nombre = 'Traian';
+
+
+if($edad >= 18 && $nombre='Traian') { //operadores > , >= , == , != diferente , ! negacion
+	echo '<h1> Bienvenido </h1>';
+}
+if($edad <= 18 && $nombre != 'Traian'){
+	echo '<h1> Eres menor de edad o no sos traian</h1>';
+}	
+
+
+?>
---
+++ conditional_else.php
@@ -0,0 +1,15 @@
+<?php
+
+$edad = 16;
+
+if ($edad >= 18){
+	echo "bienvenido";
+
+}
+else {
+	echo "no puedes ingresar";
+}
+
+
+
+?>
